7388e75de25a041dd7ec61aaea9ca36ba048caba
Maestro1982 <kris.tote@telenet.be>
2023-07-01 18:18:35 +0200

    second commit

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -37,7 +37,6 @@ const ProjectForm = ({ type, session }: Props) => {
 
     try {
       if (type === 'create') {
-        // create a new project
         await createNewProject(form, session?.user?.id, token);
         router.push('/');
       }
